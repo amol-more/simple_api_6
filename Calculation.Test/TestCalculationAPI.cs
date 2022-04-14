d0fe790bc088efeabf72de7e0fdbc7b8a284734e
amol-more <amolmore28@outlook.com>
2022-04-14 12:59:48 +0530

    Adding Tests
    Adding test cases using xUnit for Service and API.

---
+++ Calculation.Test/TestCalculationAPI.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace Calculation.Test
+{
+    public class TestCalculationAPI
+    {
+        private readonly API.Controllers.CalculationController _calculationController;
+
+        public TestCalculationAPI()
+        {
+            _calculationController = new  API.Controllers.CalculationController(new API.Services.Calculation());
+        }
+        
+        [Fact]
+        public void Test_Addition_WithTwoNumbers_ShouldAddCorrectly()
+        {
+            var addModel = new API.Model.Values();
+            addModel.Value1 = "123";
+            addModel.Value2 = "123";
+            
+            var result = _calculationController.Add(addModel);
+            
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public void Test_Addition_WithEmptyValues_ShouldReturnBadRequest()
+        {
+            var addModel = new API.Model.Values();
+            addModel.Value1 = "";
+            addModel.Value2 = "";
+
+            var result = _calculationController.Add(addModel);
+            
+            Assert.IsType<BadRequestResult>(result);
+        }
+    }
+}
---
+++ Calculation.Test/TestCalculationService.cs
@@ -0,0 +1,38 @@
+using Xunit;
+using Calculation.API;
+
+namespace Calculation.Test
+{
+    public class TestCalculationService
+    {
+        private readonly API.Services.Calculation _calculator;
+        public TestCalculationService()
+        {
+            _calculator = new API.Services.Calculation();
+        }
+
+        [Theory]
+        [InlineData("999999999999", "999999999999", "1999999999998")]
+        [InlineData("1000", "99","1099")]
+        [InlineData("28", "100072", "100100")]
+        public void Test_Calcultaion_WithTwoNumbers_ShouldAddCorrectly(string value1, string value2,string expectedValue)
+        {
+            
+            var result = _calculator.Addition(value1, value2);
+
+            Assert.True(result.Equals(expectedValue));
+        }
+        [Theory]
+        [InlineData("","")]
+        [InlineData("ABC","ABC")]
+        public void Test_Calculation_WithNoInput_ShouldReturnNullOrEmpty(string value1,string value2 )
+        {
+            
+            var result = _calculator.Addition(value1, value2);
+
+            Assert.True(string.IsNullOrEmpty(result));
+        }
+
+        
+    }
+}
